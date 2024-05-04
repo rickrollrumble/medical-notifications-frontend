3f90aaceaacc1aacd6d1ac77cf87ebaaeace660b
Rahul Ramachandran <rahul.rcd96@gmail.com>
2024-05-04 12:03:29 -0500

    refactor: formatting

--- src/components/tabs/Schedule.tsx
+++ src/components/tabs/Schedule.tsx
@@ -3,13 +3,13 @@ import { Button, Text, View } from 'react-native';
 import { styles } from 'styles';
 
 export function Schedule({navigation}:{navigation:any}): React.JSX.Element {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <View style={styles.button}>
-          <Button title="Back to Home" onPress={()=>navigation.goBack()}/>
-        </View>
-        <Button title="Go to Schedule again" onPress={()=>navigation.push('Schedule')} />
-        <Text>Schedule</Text>
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.button}>
+        <Button title="Back to Home" onPress={()=>navigation.goBack()}/>
       </View>
-    );
+      <Button title="Go to Schedule again" onPress={()=>navigation.push('Schedule')} />
+      <Text>Schedule</Text>
+    </View>
+  );
 }
