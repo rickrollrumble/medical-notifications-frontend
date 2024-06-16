2c5f5aa259ea0d718976f85dd81390014c5b9ebe
Rahul Ramachandran <rahul.rcd96@gmail.com>
2024-06-16 18:26:17 -0500

    feat: make call to external api

--- src/components/tabs/Weather/Weather.tsx
+++ src/components/tabs/Weather/Weather.tsx
@@ -1,61 +1,33 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
-import Geolocation, { GeoPosition } from 'react-native-geolocation-service';
 import { styles } from 'styles';
-import { hasLocationPermission } from 'system/permissions/LocationPermission';
+import { appLogger } from 'tools/logger';
+
+const API_KEY = '<api-key>';
+const [MKE_LAT, MKE_LON] = [43.053289, -87.891151];
+
 export function Weather(): React.JSX.Element {
-    const [location, setLocation] = useState<GeoPosition | null>(null);
-    const [highAccuracy, setHighAccuracy] = useState(true);
-    const [forceLocation, setForceLocation] = useState(true);
-    const [useLocationManager, setUseLocationManager] = useState(false);
-    const [locationDialog, setLocationDialog] = useState(true);
+    const [weather, setWeather] = useState<null | any>({});
 
     useEffect(() => {
-        async () => {
-            const hasPermission = await hasLocationPermission();
-
-            if (!hasPermission) { return true; }
-
-            Geolocation.getCurrentPosition(
-                /**
-                 * callback function for when fetching current position is successful
-                 * @param position
-                 */
-                (position) => setLocation(position),
-                /**
-                 * callback function for when fetching current position failed
-                 */
-                () => setLocation(null),
-                /**
-                 * calling options
-                 */
-                {
-                    accuracy: { android: 'high' },
-                    enableHighAccuracy: highAccuracy,
-                    distanceFilter: 0,
-                    forceRequestLocation: forceLocation,
-                    forceLocationManager: useLocationManager,
-                    showLocationDialog: locationDialog,
-                }
-            );
-        };
-    }, [forceLocation, highAccuracy, locationDialog, useLocationManager]);
+        axios.get('https://api.openweathermap.org/data/2.5/weather',
+            {
+                params: {
+                    lat: MKE_LAT,
+                    lon: MKE_LON,
+                    appid: API_KEY,
+                    units: 'imperial',
+                },
+            }
+        ).then((resp) => {
+            console.log(resp.data);
+            setWeather(resp.data);
+        }).catch((error) => appLogger.error(error));
+    }, []);
 
     return <View>
-        <Text style={styles.section}>Latitude: {location?.coords?.latitude || ''}</Text>
-        <Text style={styles.section}>Longitude: {location?.coords?.longitude || ''}</Text>
-        <Text style={styles.section}>Heading: {location?.coords?.heading}</Text>
-        <Text style={styles.section}>Accuracy: {location?.coords?.accuracy}</Text>
-        <Text style={styles.section}>Altitude: {location?.coords?.altitude}</Text>
-        <Text style={styles.section}>Altitude Accuracy: {location?.coords?.altitudeAccuracy}</Text>
-        <Text style={styles.section}>Speed: {location?.coords?.speed}</Text>
-        <Text style={styles.section}>Provider: {location?.provider || ''}</Text>
-        <Text style={styles.section}>
-            Timestamp:{' '}
-            {location?.timestamp
-                ? new Date(location.timestamp).toLocaleString()
-                : ''}
-        </Text>
+        <Text style={styles.section}>Temp: {weather.main?.temp}</Text>
+        <Text  style={styles.section}>Feels like: {weather.main?.feels_like}</Text>
     </View>;
 }
---
+++ src/tools/logger.ts
@@ -0,0 +1,3 @@
+import { logger } from 'react-native-logs';
+
+export const appLogger = logger.createLogger({ async: true });
